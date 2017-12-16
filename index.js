10a1a8e02e77eace038fabeaa7ed94ed250e3f17
Mazhar Hussain <mazharmalik10@gmail.com>
2017-12-16 16:57:38 +0500

    server-setup

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const app = express();
 const config = require('./config/database');
 const path = require('path');
 var mongoose =  require('mongoose');
-mongoose.promise = global.Promise;
-mongoose.connect(config.uri,(err)=> {
-    if(err){
-        console.log('Could Not connect to database: ', err);
-    }
-    else{
 
-        console.log('Connected to database: ', config.db);
-    }
+mongoose.Promise = require('bluebird');
 
-});
+
+mongoose.connect(config.uri, { useMongoClient: true })
+.then(() => { console.log('mongodb connected...') })
+.catch(err => { console.log(err) });
 
 app.use(express.static(__dirname + '/client/dist/'));
 
